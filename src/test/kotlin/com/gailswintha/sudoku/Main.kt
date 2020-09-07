ec774012e715297ec5e0be3b35feedd262ca62f8
Lukas Georgieff <lgeorgieff@outlook.com>
2020-09-07 21:54:15 +0200

    src: Add junit setup

---
+++ src/test/kotlin/com/gailswintha/sudoku/Main.kt
@@ -0,0 +1,11 @@
+package com.gailswintha.sudoku
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Main {
+    @Test
+    fun pass() {
+        assertThat(true).isTrue()
+    }
+}
